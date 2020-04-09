c28bd4155f9e24ca7236d06d515ead0495d9566c
Radfordhound <Grahamlrscott@gmail.com>
2020-04-09 02:26:52 -0500

    Add more to app::fnd::FileSystem

--- Sonic2013/src/app/fnd/FileSystem.cpp
+++ Sonic2013/src/app/fnd/FileSystem.cpp
@@ -1,3 +1,58 @@
 #include "FileSystem.h"
+#include "csl/fnd/String.h"
 
-// TODO
+namespace app
+{
+namespace fnd
+{
+FileSystem::FileSystem()
+{
+    SetRootDirectory("");
+}
+
+FileSystem::~FileSystem()
+{
+    // TODO
+}
+
+void FileSystem::Update() {}
+
+FileBinder* FileSystem::GetDefaultBinder() const
+{
+    return nullptr;
+}
+
+void FileSystem::SetFileCacheSize(std::size_t cacheSize)
+{
+    if (cacheSize)
+    {
+        if (!FileCache::IsInitialized())
+        {
+            Cache = nullptr;
+        }
+        else
+        {
+            FileCache::GetInstance()->Setup(cacheSize);
+            Cache = FileCache::GetInstance();
+        }
+    }
+}
+
+void FileSystem::SetRootDirectory(const char* rootDir)
+{
+    // TODO
+    csl::fnd::StrLcpy(RootDirectory, rootDir, 256);
+
+    //if (rootDir)
+    // TODO
+}
+
+void FileSystem::Setup(Info& info)
+{
+    if (info.RootDirectory)
+    {
+        SetRootDirectory(info.RootDirectory);
+    }
+}
+}
+}
--- Sonic2013/src/app/fnd/FileSystem.h
+++ Sonic2013/src/app/fnd/FileSystem.h
@@ -1,18 +1,45 @@
 #pragma once
 #include "ReferencedObject.h"
+#include "csl/fnd/Singleton.h"
+#include "FileBinder.h"
+#include "FileCache.h"
 
 namespace app
 {
 namespace fnd
 {
-class FileSystem : public ReferencedObject
+enum FILE_SYSTEM_FLAG : unsigned char
 {
-    // TODO: Data Members
+    FILE_SYSTEM_FLAG_USE_CPK = 1,
+    FILE_SYSTEM_FLAG_UK2 = 2
+    // TODO
+};
 
+class FileSystem : public ReferencedObject, public csl::fnd::Singleton<FileSystem>
+{
 public:
+    char RootDirectory[256];
+
+    // TODO: Data Members
+    
+    FileCache* Cache = nullptr;
+    FILE_SYSTEM_FLAG Flags = static_cast<FILE_SYSTEM_FLAG>(
+        FILE_SYSTEM_FLAG_USE_CPK | FILE_SYSTEM_FLAG_UK2);
+
+    // TODO: Data Members (Actually I think there aren't any more here)
+
+    struct Info // size == 8
+    {
+        const char* RootDirectory = nullptr;
+        bool fx05 = true;
+        bool fx06 = true;
+
+        // TODO: Are the remaining 2 bytes for sure just padding?
+    };
+
     /*
         @brief Constructor for FileSystem.
-        <b>Wii U: TODO, PC: TODO</b>
+        <b>Wii U: 0x0218eb24, PC: TODO</b>
     */
     FileSystem();
 
@@ -21,6 +48,21 @@ public:
         <b>Wii U: TODO, PC: TODO</b>
     */
     ~FileSystem() override;
+
+    /* <b>Wii U: TODO, PC: TODO</b> */
+    virtual void Update();
+
+    /* <b>Wii U: TODO, PC: TODO</b> */
+    virtual FileBinder* GetDefaultBinder() const;
+
+    /* <b>Wii U: 0x0218ead0, PC: TODO</b> */
+    void SetFileCacheSize(std::size_t cacheSize);
+
+    /* <b>Wii U: 0x0218e5c8, PC: TODO</b> */
+    void SetRootDirectory(const char* rootDir);
+
+    /* <b>Wii U: TODO, PC: TODO</b> */
+    void Setup(Info& info);
 
     // TODO
 };
