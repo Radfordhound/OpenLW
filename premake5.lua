d719f099c3a378f7050d6cdcb2234a0138e41d5e
Radfordhound <Grahamlrscott@gmail.com>
2020-04-23 18:30:32 -0500

    Disable strcpy_s, wcscpy_s, etc. warnings on Windows they're very annoying

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,11 @@
 workspace("OpenLW")
     configurations({ "Debug", "Release" })
     platforms({ "x86", "x64" })
+
+    -- Disable some warnings on windows
+    if os.target() == "windows" then
+        disablewarnings("4996") -- please use wcscpy_s even though it's not in the C++ standard we're begging you
+    end
     
 include("Library/cslib")
 include("Sonic2013")
