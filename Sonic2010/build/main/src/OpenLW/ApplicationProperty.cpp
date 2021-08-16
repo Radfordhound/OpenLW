f88f6ca0279f628c1908a609c394f2eb8c848818
Graham Scott <Grahamlrscott@gmail.com>
2021-08-15 21:10:33 -0500

    Start implementing ApplicationProperty

---
+++ Sonic2010/build/main/src/OpenLW/ApplicationProperty.cpp
@@ -0,0 +1,31 @@
+#include "ApplicationProperty.h"
+#include "fnd/Allocators.h"
+#include "ApplicationPropertyImpl.h"
+
+using namespace app::fnd;
+
+namespace app
+{
+ApplicationProperty::ApplicationProperty()
+{
+    m_impl = new ApplicationPropertyImpl(m_allocator);
+}
+
+// Wii U: 0x021c8354, PC: TODO
+void* ApplicationProperty_init()
+{
+    ApplicationProperty* appProperty = new (GetSingletonAllocator()) ApplicationProperty();
+    appProperty->AddRef();
+    return appProperty;
+}
+
+// Wii U: 0x021c83a4 (THUNK), PC: TODO
+void ApplicationProperty_destroy(void* ptr)
+{
+    static_cast<ApplicationProperty*>(ptr)->Release();
+}
+
+DEFINE_SINGLETON(ApplicationProperty,
+    ApplicationProperty_init,
+    ApplicationProperty_destroy);
+}
