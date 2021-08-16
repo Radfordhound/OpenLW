cf8fb97c67f1f78e6dbf61b5041def2dbd88e8f5
Graham Scott <Grahamlrscott@gmail.com>
2021-08-15 20:55:39 -0500

    Implement more of HH CDatabaseData

--- Sonic2013/Hedgehog/build/Database/source/hhCDatabaseData.cpp
+++ Sonic2013/Hedgehog/build/Database/source/hhCDatabaseData.cpp
@@ -4,6 +4,20 @@ namespace hh
 {
 namespace db
 {
+bool CDatabaseData::IsMadeAllInternal() const
+{
+    // TODO: Why is this function marked const in the symbols, yet does non-const stuff??
+
+    if ((m_flags & DB_MADE_ONE) != 0 &&
+        const_cast<CDatabaseData*>(this)->CheckMadeAll())
+    {
+        const_cast<CDatabaseData*>(this)->m_flags |= DB_MADE_ALL;
+        return true;
+    }
+    
+    return false;
+}
+
 CDatabaseData::CDatabaseData() :
     m_flags(DB_UNKNOWN_FLAG4),
     field_0x4()
@@ -19,6 +33,11 @@ CDatabaseData::~CDatabaseData()
 void CDatabaseData::SetMadeOne()
 {
     m_flags |= DB_MADE_ONE;
+}
+
+void CDatabaseData::SetMadeAllFlag()
+{
+    m_flags |= DB_MADE_ALL;
 }
 
 bool CDatabaseData::CheckMadeAll()
--- Sonic2013/Hedgehog/include/Hedgehog/Database/hhCDatabaseData.h
+++ Sonic2013/Hedgehog/include/Hedgehog/Database/hhCDatabaseData.h
@@ -9,6 +9,7 @@ namespace db
 enum DatabaseFlags
 {
     DB_MADE_ONE = 1,
+    DB_MADE_ALL = 2,
     DB_UNKNOWN_FLAG4 = 4
 };
 
@@ -18,15 +19,27 @@ class CDatabaseData : public base::CObject
     // TODO: Other data members? Or is it just padding?
     base::CSharedString field_0x4;
 
+    // Wii U: 0x036c46ec, PC: TODO
+    bool IsMadeAllInternal() const;
+
 public:
     // Wii U: 0x036c4764, PC: TODO
     CDatabaseData();
+
+    bool IsMadeAll() const
+    {
+        return ((m_flags & DB_MADE_ALL) != 0 ||
+            IsMadeAllInternal());
+    }
 
     // Wii U: 0x036c47cc, PC: TODO
     virtual ~CDatabaseData();
 
     // Wii U: 0x036c4850, PC: TODO
     void SetMadeOne();
+
+    // Wii U: 0x036c489c, PC: TODO
+    void SetMadeAllFlag();
 
     // Wii U: 0x036c48c4, PC: TODO
     virtual bool CheckMadeAll();
