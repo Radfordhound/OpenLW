09efd11bcadb05b281f966c6b34ea0495cd43509
Graham Scott <Grahamlrscott@gmail.com>
2021-08-15 19:59:36 -0500

    Implement more RsdxEvent stuff

--- Sonic2013/Hedgehog/build/Rsdx/source/System/RsdxEvent.cpp
+++ Sonic2013/Hedgehog/build/Rsdx/source/System/RsdxEvent.cpp
@@ -13,13 +13,57 @@ RsdxEventHandle RsdxCreateEvent(unsigned int param_1, unsigned int param_2)
 #endif
 }
 
+void RsdxDestroyEvent(RsdxEventHandle event)
+{
+#ifdef _WIN32
+    CloseHandle(event); // TODO: Do we return this actually?
+#elif defined(__WIIU__)
+    // TODO
+#endif
+}
+
+bool RsdxResetEvent(RsdxEventHandle event)
+{
+#ifdef _WIN32
+    return (ResetEvent(event) != 0);
+#elif defined(__WIIU__)
+    OSResetEvent(event);
+    return true;
+#endif
+}
+
 bool RsdxSetEvent(RsdxEventHandle event)
 {
 #ifdef _WIN32
-    return (SetEvent(event) != 0);
+    BOOL r = SetEvent(event);
+    return (!r);
 #elif defined(__WIIU__)
     OSSignalEvent(event);
     return true;
+#endif
+}
+
+bool RsdxWaitEvent(RsdxEventHandle event, unsigned int timeout)
+{
+#ifdef _WIN32
+    return (WaitForSingleObject(event, timeout) == WAIT_OBJECT_0);
+#elif defined(__WIIU__)
+    if (event != -1)
+    {
+        return (OSWaitEventWithTimeout(event, timeout * 1000) != 0);
+    }
+
+    OSWaitEvent(event);
+    return true;
+#endif
+}
+
+bool RsdxCheckEvent(RsdxEventHandle event)
+{
+#ifdef _WIN32
+    return (WaitForSingleObject(event, 0) == WAIT_OBJECT_0);
+#elif defined(__WIIU__)
+    return (OSWaitEventWithTimeout(event, 0) != 0);
 #endif
 }
 }
--- Sonic2013/Hedgehog/include/Hedgehog/Rsdx/System/RsdxEvent.h
+++ Sonic2013/Hedgehog/include/Hedgehog/Rsdx/System/RsdxEvent.h
@@ -20,7 +20,19 @@ typedef OSEvent* RsdxEventHandle;
 // Wii U: 0x036a7b24, PC: 0x00c25ad0
 RsdxEventHandle RsdxCreateEvent(unsigned int param_1, unsigned int param_2);
 
+// Wii U: 0x036a7b80, PC: 0x00c25af0
+void RsdxDestroyEvent(RsdxEventHandle event);
+
+// Wii U: 0x036a7b8c, PC: 0x00c25b00
+bool RsdxResetEvent(RsdxEventHandle event);
+
 // Wii U: 0x036a7bb0, PC: 0x00c25b20
 bool RsdxSetEvent(RsdxEventHandle event);
+
+// Wii U: 0x036a7bd4, PC: 0x00c25b40
+bool RsdxWaitEvent(RsdxEventHandle event, unsigned int timeout);
+
+// Wii U: 0x036a7c24, PC: 0x00c25b60
+bool RsdxCheckEvent(RsdxEventHandle event);
 }
 }
