ec9d18cf7a3835901e5bf070a7ea0d1647dbec74
Graham Scott <Grahamlrscott@gmail.com>
2023-03-22 13:42:27 -0500

    Fix typo in bitset.h and improve naming

--- Library/cslib/include/csl/ut/bitset.h
+++ Library/cslib/include/csl/ut/bitset.h
@@ -6,10 +6,10 @@ namespace csl
 namespace ut
 {
 /** @brief A helpful wrapper for dealing with collections of bits. */
-template<typename T, int bitCount = CSL_BIT_COUNT(T)>
+template<typename T, int BitCount = CSL_BIT_COUNT(T)>
 class Bitset
 {
-    T m_data;
+    T m_Data;
 
 public:
     /**
@@ -18,7 +18,7 @@ public:
     */
     bool any() const
     {
-        return (m_data != 0);
+        return (m_Data != 0);
     }
 
     /**
@@ -39,9 +39,9 @@ public:
         T bitMask(1);
         unsigned int count = 0;
 
-        for (int pos = bitCount; pos != 0; --pos)
+        for (int pos = BitCount; pos != 0; --pos)
         {
-            if ((m_data & bitMask))
+            if ((m_Data & bitMask))
             {
                 ++count;
             }
@@ -65,9 +65,9 @@ public:
         @brief Flips the value of the bit at the given index (i.e. if the given bit is 0 it becomes 1).
         @param[in] bitIndex The zero-based index of the bit to flip within the bitset.
     */
-    Bitset<T, bitCount> flip(unsigned int bitIndex)
+    Bitset<T, BitCount> flip(unsigned int bitIndex)
     {
-        m_data ^= static_cast<T>(1 << bitIndex);
+        m_Data ^= static_cast<T>(1 << bitIndex);
         return *this;
     }
 
@@ -75,20 +75,20 @@ public:
         @brief Flips the value of the bit at the given index (i.e. if the given bit is 0 it becomes 1).
         @param[in] bitIndex The zero-based index of the bit to flip within the bitset.
     */
-    inline Bitset<T, bitCount> Flip(unsigned int bitIndex)
+    inline Bitset<T, BitCount> Flip(unsigned int bitIndex)
     {
         return flip(bitIndex);
     }
 
     /** @brief Resets all bits in the bitset to 0. */
-    Bitset<T, bitCount> reset()
+    Bitset<T, BitCount> reset()
     {
-        m_data = 0;
+        m_Data = 0;
         return *this;
     }
 
     /** @brief Resets all bits in the bitset to 0. */
-    inline Bitset<T, bitCount> Reset()
+    inline Bitset<T, BitCount> Reset()
     {
         return *this;
     }
@@ -97,9 +97,9 @@ public:
         @brief Sets the bit at the given index to 0.
         @param[in] bitIndex The zero-based index of the bit to disable within the bitset.
     */
-    Bitset<T, bitCount> reset(unsigned int bitIndex)
+    Bitset<T, BitCount> reset(unsigned int bitIndex)
     {
-        m_data &= ~static_cast<T>(1 << bitIndex);
+        m_Data &= ~static_cast<T>(1 << bitIndex);
         return *this;
     }
 
@@ -107,20 +107,20 @@ public:
         @brief Sets the bit at the given index to 0.
         @param[in] bitIndex The zero-based index of the bit to disable within the bitset.
     */
-    inline Bitset<T, bitCount> Reset(unsigned int bitIndex)
+    inline Bitset<T, BitCount> Reset(unsigned int bitIndex)
     {
-        return reset(pos);
+        return reset(bitIndex);
     }
 
     /** @brief Sets all bits in the bitset to 1. */
-    Bitset<T, bitCount> set()
+    Bitset<T, BitCount> set()
     {
-        m_data = (std::numeric_limits<T>::max)();
+        m_Data = (std::numeric_limits<T>::max)();
         return *this;
     }
 
     /** @brief Sets all bits in the bitset to 1. */
-    inline Bitset<T, bitCount> Set()
+    inline Bitset<T, BitCount> Set()
     {
         return set();
     }
@@ -129,9 +129,9 @@ public:
         @brief Sets the bit at the given index to 1.
         @param[in] bitIndex The zero-based index of the bit to enable within the bitset.
     */
-    Bitset<T, bitCount> set(unsigned int bitIndex)
+    Bitset<T, BitCount> set(unsigned int bitIndex)
     {
-        m_data |= static_cast<T>(1 << bitIndex);
+        m_Data |= static_cast<T>(1 << bitIndex);
         return *this;
     }
 
@@ -139,9 +139,9 @@ public:
         @brief Sets the bit at the given index to 1.
         @param[in] bitIndex The zero-based index of the bit to enable within the bitset.
     */
-    inline Bitset<T, bitCount> Set(unsigned int bitIndex)
+    inline Bitset<T, BitCount> Set(unsigned int bitIndex)
     {
-        return set(pos);
+        return set(bitIndex);
     }
 
     /**
@@ -149,7 +149,7 @@ public:
         @param[in] bitIndex The zero-based index of the bit to modify within the bitset.
         @param[in] val      The value to set the given bit to (false == 0 and true == 1).
     */
-    Bitset<T, bitCount> set(unsigned int bitIndex, bool val)
+    Bitset<T, BitCount> set(unsigned int bitIndex, bool val)
     {
         return (val) ? set(bitIndex) : reset(bitIndex);
     }
@@ -159,7 +159,7 @@ public:
         @param[in] bitIndex The zero-based index of the bit to modify within the bitset.
         @param[in] val      The value to set the given bit to (false == 0 and true == 1).
     */
-    inline Bitset<T, bitCount> Set(unsigned int bitIndex, bool val)
+    inline Bitset<T, BitCount> Set(unsigned int bitIndex, bool val)
     {
         return set(bitIndex, val);
     }
@@ -171,7 +171,7 @@ public:
     */
     bool test(unsigned int bitIndex) const
     {
-        return ((m_data >> bitIndex) & 1);
+        return ((m_Data >> bitIndex) & 1);
     }
 
     /**
@@ -186,17 +186,17 @@ public:
 
     unsigned int to_ulong() const
     {
-        return static_cast<unsigned int>(m_data);
+        return static_cast<unsigned int>(m_Data);
     }
 
     Bitset() :
-        m_data(0) {}
+        m_Data(0) {}
 
     Bitset(T data) :
-        m_data(data) {}
+        m_Data(data) {}
 
-    Bitset(const Bitset<T, bitCount>& other) :
-        m_data(other.m_data) {}
+    Bitset(const Bitset<T, BitCount>& other) :
+        m_Data(other.m_Data) {}
 };
 }
 }
