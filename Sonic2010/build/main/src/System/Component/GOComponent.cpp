9dd9872e590c443722e71563513ea6b618da037e
Graham Scott <Grahamlrscott@gmail.com>
2023-03-24 00:32:55 -0500

    Fill out GameObject and GOComponent

--- Sonic2010/build/main/src/System/Component/GOComponent.cpp
+++ Sonic2010/build/main/src/System/Component/GOComponent.cpp
@@ -1,4 +1,19 @@
 #include "OpenLW/pch.h"
 #include "GOComponent.h"
+#include "System/GameObject.h"
 
-// TODO
+namespace app
+{
+namespace fnd
+{
+GOComponent* GOComponent::GetComponentByID(const char* id)
+{
+    return m_activeObject->GetGOC(id);
+}
+
+const GOComponent* GOComponent::GetComponentByID(const char* id) const
+{
+    return m_activeObject->GetGOC(id);
+}
+} // fnd
+} // app
--- Sonic2010/build/main/src/System/GameObject.cpp
+++ Sonic2010/build/main/src/System/GameObject.cpp
@@ -1,1 +1,17 @@
 #include "OpenLW/pch.h"
+#include "GameObject.h"
+
+namespace app
+{
+fnd::GOComponent* GameObject::GetGOC(const char* id)
+{
+    // TODO
+    return nullptr;
+}
+
+const fnd::GOComponent* GameObject::GetGOC(const char* id) const
+{
+    // TODO
+    return nullptr;
+}
+} // app
