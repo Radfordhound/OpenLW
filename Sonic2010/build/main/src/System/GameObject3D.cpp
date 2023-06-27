6578ce5d58c45e2b5ea8b74d8ed30b88f4b651b1
Graham Scott <Grahamlrscott@gmail.com>
2023-06-27 17:39:10 -0500

    Add placeholder GameObject3D

---
+++ Sonic2010/build/main/src/System/GameObject3D.cpp
@@ -0,0 +1,42 @@
+﻿// TODO: The name of this file is correct, but the path was guessed!
+#include "OpenLW/pch.h"
+#include "GameObject3D.h"
+
+namespace app
+{
+bool GameObject3D::ProcessMessage(fnd::Message& msg)
+{
+    // TODO
+    return false;
+}
+
+GameObject3D::~GameObject3D()
+{
+    // TODO
+}
+
+void GameObject3D::ProcMsgDeactivate(xgame::MsgDeactivate& msg)
+{
+    // TODO
+}
+
+void GameObject3D::ProcMsgGetPosition(xgame::MsgGetPosition& msg)
+{
+    // TODO
+}
+
+void GameObject3D::ProcMsgGetRotation(xgame::MsgGetRotation& msg)
+{
+    // TODO
+}
+
+void GameObject3D::ProcMsgActivateObjectDebugWindow(xgame::MsgActivateObjectDebugWindow& msg)
+{
+    // TODO
+}
+
+GameObject3D::GameObject3D()
+{
+    // TODO
+}
+} // app
