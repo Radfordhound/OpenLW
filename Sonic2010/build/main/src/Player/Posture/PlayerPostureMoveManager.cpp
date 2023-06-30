6b935572d845784cd3cfbc02bb8a01793462a658
Graham Scott <Grahamlrscott@gmail.com>
2023-06-30 02:33:31 -0500

    Add placeholder CPostureMoveManager

---
+++ Sonic2010/build/main/src/Player/Posture/PlayerPostureMoveManager.cpp
@@ -0,0 +1,4 @@
+#include "OpenLW/pch.h"
+#include "PlayerPostureMoveManager.h"
+
+// TODO
---
+++ Sonic2010/build/main/src/Player/Posture/PlayerPostureMoveManager.h
@@ -0,0 +1,36 @@
+// TODO: Both the name of this file and its path were guessed!
+#pragma once
+#include "PlayerPostureManager.h"
+
+namespace app
+{
+namespace Player
+{
+class CPostureMoveManager : public CPostureManager
+{
+public:
+    LWAPI(0x0292CFFC, TODO)
+    ~CPostureMoveManager();
+
+    LWAPI(0x0292CF80, TODO)
+    void Update(float dt);
+
+    LWAPI(0x0292CFF8, TODO)
+    void BeforCalculate(float param_1);
+
+    LWAPI(0x0292BAA0, TODO)
+    CPostureMoveManager(CPlayer* player, CPhysics* physics);
+
+    LWAPI(0x0292C0DC, TODO)
+    void RegisterCommonPosture();
+
+    LWAPI(0x0292CF7C, TODO)
+    void AfterCalculate(float param_1);
+
+    LWAPI(0x0292CF48, TODO)
+    void SetupPosture();
+};
+
+LWAPI_STATIC_ASSERT_SIZE(CPostureMoveManager, 0x38)
+} // Player
+} // app
