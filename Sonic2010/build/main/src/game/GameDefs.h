5b1b8ce8a4fddfe4516c3a80045db42c4b1059f1
Graham Scott <Grahamlrscott@gmail.com>
2023-07-01 04:49:27 -0500

    Move EPhantomType to GameDefs.h

---
+++ Sonic2010/build/main/src/game/GameDefs.h
@@ -0,0 +1,13 @@
+// TODO: The name of this file is correct, but the path was guessed!
+#pragma once
+
+namespace app
+{
+namespace Game
+{
+enum EPhantomType
+{
+    // TODO
+};
+} // Game
+} // app
